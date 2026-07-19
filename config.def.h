2e6f7dc7c2023f52ee43a6d94d30b3d932ae95b6
Aarvsn <aarvsn@disroot.org>
2026-07-19 20:19:41 +0500

    Refactor config.def.h for key bindings and commands

--- config.def.h
+++ config.def.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <X11/XF86keysym.h>
+
 #define MOD Mod4Mask
 
 /* Gap (in pixels) reserved at the top of the screen, e.g. for a status bar.
@@ -10,39 +12,41 @@
 
 /* Name (or name prefix) of a persistent bar window that must NOT be unmapped
    when switching workspaces (e.g. a lemonbar titled "bar"). */
-const char* barname = "bar";
+
+static const char* barname   = "bar";
+
 #define BORDER_SELECT   "#880000"
 #define BORDER_NORMAL   "#333333"
 #define BORDER_WIDTH    1
 
-const char* menu[]    = {"dmenu_run",      0};
-const char* term[]    = {"st",             0};
-const char* scrot[]   = {"scr",            0};
-const char* briup[]   = {"bri", "10", "+", 0};
-const char* bridown[] = {"bri", "10", "-", 0};
-const char* voldown[] = {"amixer", "sset", "Master", "5%-",         0};
-const char* volup[]   = {"amixer", "sset", "Master", "5%+",         0};
-const char* volmute[] = {"amixer", "sset", "Master", "toggle",      0};
-const char* colors[]  = {"bud", "/home/goldie/Pictures/Wallpapers", 0};
+static const char* menu[]    = {"dmenu_run",      0};
+static const char* term[]    = {"st",             0};
+static const char* scrot[]   = {"scr",            0};
+static const char* briup[]   = {"bri", "10", "+", 0};
+static const char* bridown[] = {"bri", "10", "-", 0};
+static const char* voldown[] = {"amixer", "sset", "Master", "5%-",    0};
+static const char* volup[]   = {"amixer", "sset", "Master", "5%+",    0};
+static const char* volmute[] = {"amixer", "sset", "Master", "toggle", 0};
+static const char* colors[]  = {"bud", "/home/goldie/Pictures/Wallpapers", 0};
 
-static struct key keys[] = {
+static key keys[] = {
     {MOD,      XK_q,   win_kill,   {0}},
     {MOD,      XK_c,   win_center, {0}},
     {MOD,      XK_f,   win_fs,     {0}},
 
-    {Mod1Mask,           XK_Tab, win_next,   {0}},
-    {Mod1Mask|ShiftMask, XK_Tab, win_prev,   {0}},
+    {Mod1Mask,           XK_Tab, win_next, {0}},
+    {Mod1Mask|ShiftMask, XK_Tab, win_prev, {0}},
 
     {MOD, XK_d,      run, {.com = menu}},
     {MOD, XK_w,      run, {.com = colors}},
     {MOD, XK_p,      run, {.com = scrot}},
     {MOD, XK_Return, run, {.com = term}},
 
-    {0,   XF86XK_AudioLowerVolume,  run, {.com = voldown}},
-    {0,   XF86XK_AudioRaiseVolume,  run, {.com = volup}},
-    {0,   XF86XK_AudioMute,         run, {.com = volmute}},
-    {0,   XF86XK_MonBrightnessUp,   run, {.com = briup}},
-    {0,   XF86XK_MonBrightnessDown, run, {.com = bridown}},
+    {0, XF86XK_AudioLowerVolume,  run, {.com = voldown}},
+    {0, XF86XK_AudioRaiseVolume,  run, {.com = volup}},
+    {0, XF86XK_AudioMute,         run, {.com = volmute}},
+    {0, XF86XK_MonBrightnessUp,   run, {.com = briup}},
+    {0, XF86XK_MonBrightnessDown, run, {.com = bridown}},
 
     {MOD,           XK_1, ws_go,     {.i = 1}},
     {MOD|ShiftMask, XK_1, win_to_ws, {.i = 1}},
